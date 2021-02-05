ecb08d0dfa0ec904d11f261f076e1bfa0568c1c8
Paulo Henrique Congentino Ten <phcten@gmail.com>
2021-02-05 12:59:23 -0300

    Changed the ''Horse'' piece to it's proper name (Knight), defined the lether N to it and implemented the knight movment rules

---
+++ src/Chess/Pieces/Knight.java
@@ -0,0 +1,98 @@
+package Chess.Pieces;
+
+import Boardgame.Board;
+import Boardgame.Position;
+import Chess.ChessPiece;
+import Chess.Color;
+
+public class Knight extends ChessPiece {
+
+    public Knight(Board board, Color color){
+        super(board,color);
+    }
+    @Override
+    public String toString(){
+        return "N";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getRows()];
+
+        Position p = new Position(0,0);
+
+        //2 Up 1 Left
+        p.setValues(position.getRow() -1, position.getColumn()-2);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //2 Up 1 Right
+        p.setValues(position.getRow() +1, position.getColumn()-2);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //2 Left 1 Up
+        p.setValues(position.getRow() -2, position.getColumn()-1);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //2 left 1 Down
+        p.setValues(position.getRow() -2, position.getColumn()+1);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //2 Down 1 left
+        p.setValues(position.getRow() -1, position.getColumn()+2);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //2 Down 1 Right
+        p.setValues(position.getRow() +1, position.getColumn()+2);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //2 Right 1 Up
+        p.setValues(position.getRow() +2, position.getColumn()-1);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //2 Right 1 Down
+        p.setValues(position.getRow() +2, position.getColumn()+1);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        return mat;
+    }
+}
